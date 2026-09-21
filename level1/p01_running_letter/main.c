14b32706268b2abcee31f79ee0e82edc5a247587
jianmoguanyan <3562294142@qq.com>
2026-09-21 15:13:20 +0800

    金鑫涵的作业level1.p01
    在课堂练习的基础上做出部分修改，优化了字母从右往左走时会有残留字母的问题

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <windows.h>
 
-int main() {
-    printf("hello world!\n");
-    return 0;
-}
+int times = 0;
+int k= 0;
+int i;
+
+int main()
+{
+	while (times <= 30)
+	{
+		for (int i = 0; i<= times;i++)		
+		{
+			printf(" ");
+		}
+		fflush(stdout);
+		printf("A");
+		Sleep(200);
+		fflush(stdout);
+		printf("\r                                             \r");
+		times++;
+	}
+	//现在times=31 i=0//
+	while (k<=30)
+	{
+		for (i=30; i>= k;i--)
+		{
+			printf(" ");
+		}
+		fflush(stdout);
+		printf("A");
+		printf("                                                     \r");
+		Sleep(200);
+		fflush(stdout);
+		printf("                                             \r");
+		k++;
+	}
+
+	printf("over");
+	return 0;
