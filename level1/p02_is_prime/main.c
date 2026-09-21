6b6287d0b93a45d9ec5622967e64278e019bb695
jianmoguanyan <3562294142@qq.com>
2026-09-21 21:30:39 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -10,14 +10,31 @@ int box3[99999];
 
 int main(void)
 {   
-	double count = 0;
+	int count = 0;
 	printf("Input A Positive Integer :");
 	scanf("%d", &number);
-	if (number % 2 != 0) 
+
+	if (number == 1)
+	{
+		printf("%d is not a prime number.", number);
+		return 0;
+	}
+	else if (number == 2) 
+	{
+		printf("%d is a prime number.", number);
+		return 0;
+	}
+    else if (number % 2 == 0)
+	{
+		printf("%d is not a prime number.", number);
+		return 0;
+	}
+	//到此已排除1、2、偶数三种特殊情况
+	else 
 	{
 		count = sqrt(number);
 		int k = 0;
-		for (int i = 1;i < count;i++)
+		for (int i = 2;i <= count;i++)
 		{
 			box1[k] = i;
 			k++;
@@ -42,31 +59,29 @@ int main(void)
 		//printf("%d ", box2[p]);
 		//可以使用此代码测试奇数盒box2是否正常录入数字
 
+		int right;
 		double real = 0;
 		for (int e = 0;e < n;e++)
 		{
 			box3[e] = number % box2[e];
-			if (box3[e] != 0)
+			if (box3[e] == 0)
 			{
-				real = real + 1;
+				printf("%d is  not a prime number.", number);
+				right++;
+				break;
 			}
-			else
-			{
-				real = real + 0;
-			}
+
 		}
 
-		if (real == 0)
+		if (right != 0)
+		{
+			return 0;
+		}
+		else
 		{
 			printf("%d is a prime number.", number);
 		}
-		else if (real != 0)
-		{
-			
-		}
 	}
-	else if(number%2==0)
-	{ printf("%d is not a prime number.", number);}
 	return 0;
 }
 //使用VS C语言环境完成//
