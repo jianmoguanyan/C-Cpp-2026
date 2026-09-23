95ccb1f99b020dbb172ed40c1c7592ef4808f944
jianmoguanyan <3562294142@qq.com>
2026-09-23 20:34:26 +0800

    01_跑动的字母_第一次修改

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,41 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <windows.h>
 
-int times = 0;
-int k= 0;
-int i;
+#define TEXT "A"
+#define SCREEN_WIDTH 80
+#define SPEED_MS 50
 
-int main() {
-	while (times <= 30)
-	{
-		for (int i = 0; i<= times;i++)		
-		{
-			printf(" ");
-		}
-		fflush(stdout);
-		printf("A");
-		Sleep(200);
-		fflush(stdout);
-		printf("\r                                             \r");
-		times++;
-	}
-	//现在times=31 i=0//
-	while (k<=30)
-	{
-		for (i=30; i>= k;i--)
-		{
-			printf(" ");
-		}
-		fflush(stdout);
-		printf("A");
-		printf("                                                     \r");
-		Sleep(200);
-		fflush(stdout);
-		printf("                                             \r");
-		k++;
-	}
+int main(void)
+{
+	int pos = 0;        // 当前字符起始位置
+	int dir = 1;        // 移动方向 1向右，-1向左
+	int textLen = strlen(TEXT);
+	int maxPos = SCREEN_WIDTH - textLen;  // 字符能到达的最右侧坐标
 
-	printf("over");
+	while (1)
+	{
+		system("cls");
+		// 打印前置空格实现字符偏移
+		for (int i = 0; i < pos; i++)
+		{
+			putchar(' ');
+		}
+		printf("%s\n", TEXT);
+
+		pos += dir;
+
+		// 碰到右边界，反向向左
+		if (pos >= maxPos)
+		{
+			pos = maxPos;
+			dir = -1;
+		}
+		// 碰到左边界，反向向右
+		if (pos <= 0)
+		{
+			pos = 0;
+			dir = 1;
+		}
+
+		Sleep(SPEED_MS);
+	}
 	return 0;
 }
-  //
+
+//VS C环境
